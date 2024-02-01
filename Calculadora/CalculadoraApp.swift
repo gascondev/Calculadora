59a8a2a951d7d3a063a987123240008613681df4
Álvaro Gascón <alvarogascon@hotmail.com>
2024-02-01 08:49:25 +0100

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Álvaro Gascón on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
